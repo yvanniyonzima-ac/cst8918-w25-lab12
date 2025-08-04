214261e259caf09282790f486843fa544c84532a
supersuper2 <sing1883@algonquinlive.com>
2025-08-04 14:00:40 -0400

    created new branch and added vnet/subnet to tf-app

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -2,3 +2,21 @@ resource "azurerm_resource_group" "app_rg" {
   name     = "${var.college_id}-a12-rg"
   location = "Canada Central"
 }
+
+resource "azurerm_virtual_network" "app_vnet" {
+  name                = "${var.college_id}-a12-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "${var.college_id}-a12-subnet"
+  resource_group_name  = azurerm_resource_group.app_rg.name
+  virtual_network_name = azurerm_virtual_network.app_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
